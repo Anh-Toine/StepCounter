9f0945b02ce446d443bc014fccd82e8dd2f35ffd
Anh-Toine <52642269+Anh-Toine@users.noreply.github.com>
2022-09-02 12:35:41 -0400

    Initial Commit

---
+++ StepCounter/StepCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepCounterApp.swift
+//  StepCounter
+//
+//  Created by Lukas Nguyen on 2022-09-02.
+//
+
+import SwiftUI
+
+@main
+struct StepCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
